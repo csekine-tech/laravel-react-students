3bb30b8c08486fd947e167068a6367f8b4d06e0c
csekine-tech <csekine.mscll24@gmail.com>
2022-04-19 21:13:11 +0900

    basic, ClassComponent -> FunctionalComponent

--- laravel-students-api/app/Http/Controllers/API/StudentController.php
+++ laravel-students-api/app/Http/Controllers/API/StudentController.php
@@ -16,6 +16,27 @@ class StudentController extends Controller
         ]);
     }
 
+    public function edit($id){
+        $student = Student::find($id);
+        return response()->json([
+            'status' => 200,
+            'student' =>$student
+        ]);
+    }
+
+    public function update($id,Request $request){
+        $student = Student::find($id);
+        $student->name = $request->input('name');
+        $student->course = $request->input('course');
+        $student->email = $request->input('email');
+        $student->phone = $request->input('phone');
+        $student->save();
+        return response()->json([
+            'status' => 200,
+            'message' => 'student updated successfully!'
+        ]);
+    }
+
     public function store(Request $request){
         $student = new Student;
         $student->name = $request->input('name');
@@ -27,6 +48,14 @@ class StudentController extends Controller
         return response()->json([
             'status' => 200,
             'message' => 'student added successfully!'
+        ]);
+    }
+
+    public function delete($id){
+        Student::find($id)->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'student removed successfully!'
         ]);
     }
 }
--- laravel-students-api/routes/api.php
+++ laravel-students-api/routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\API\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 Route::get('/students',[StudentController::class,'index']);
+Route::get('/student/{id}',[StudentController::class,'edit']);
+Route::post('/update-student/{id}',[StudentController::class,'update']);
 Route::post('/add-student',[StudentController::class,'store']);
-
+Route::get('/remove-student/{id}',[StudentController::class,'delete']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
